69564117b33226bbadc49fff3603e90cad6776fd
Dale Puckett <dale.puckett@att.net>
2016-09-25 00:39:15 -0500

    Working up to Navigation Bar and Back Button

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Dale Puckett on 9/24/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
